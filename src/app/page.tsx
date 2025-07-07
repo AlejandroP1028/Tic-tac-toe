eb4864567d29f99f205060aef5a0fa9f6f026d5c
Alejandro Prado <alejandroprado885@gmail.com>
2025-07-07 21:11:14 +0800

    finished building ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import BoxParent from "@/components/BoxParent";
+import { SiNextdotjs, SiGreensock } from "react-icons/si";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center space-y-10">
+      <h1 className="text-4xl font-medium">Alejandro's Tic-Tac-Toe</h1>
+      <BoxParent />
+      <div className="text-lg flex flex-col items-center">
+        <h4 className="font-semibold">Built with:</h4>
+        <div className="flex flex-row gap-4 items-center mt-4">
+          <div className="flex flex-row gap-2 items-center">
+            {" "}
+            Next.js{" "}
+            <SiNextdotjs className="w-8 h-8 text-black" title="Next.js" />
+          </div>
+          <div className="flex flex-row gap-2 items-center">
+            {" "}
+            GSAP <SiGreensock className="w-8 h-8 text-black" title="GSAP" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
